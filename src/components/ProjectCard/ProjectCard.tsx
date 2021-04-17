6606753955e25111fe516140282550273c181712
nicolas74 <nicolas.savoini@mac.com>
2021-04-16 20:52:22 -0400

    align two component for project card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,29 +1,42 @@
+import AspectRatio from 'react-aspect-ratio'; 
 import React from 'react';
-import { DivBackground} from './styles';
+
+import { DivBackground, }  from './styles';
 
 
 type ProjectCardProps = {
     title: string
 }
 
-
 const clickButton = () => {
     console.log('toto b');
 }
 
+
 function ProjectCard({ title }: ProjectCardProps) {
     return (
-    
-    <DivBackground>
-        <div>Text</div>
+      
+            <DivBackground>
+                <div>{title} </div>
+                
 
-        <div>{title} </div>
+                <div style={{ width: '100%', display: 'flex' }}>
 
+                    <div style={{ width: '50%' }}>
+                        TEXT en Bas
+                    </div>
 
-        <div><button onClick={clickButton}>Clic</button></div>
+                    <div style={{ width: '50%' }}>
+                        <button onClick={clickButton}>Clic</button>
+                    </div>
 
+                </div>
+            </DivBackground>
 
-    </DivBackground>);
+      );
 }
 
 export default ProjectCard;
+/*
+<AspectRatio ratio="0.5" style={{  maxWidth: '400px', backgroundImage: 'url("./ProjectCardDefaultImage.jpg")' }}>
+*/
--- src/components/ProjectCard/styles.ts
+++ src/components/ProjectCard/styles.ts
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import defaultImage from './ProjectCardDefaultImage.jpg';
 
+
 export const DivBackground = styled.div`
     border: 1px solid #000;
-    background-image: url(${ defaultImage});
 `;
+
+//background-image: url(${ defaultImage});
